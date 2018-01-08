e512b8ed150b633ec1c515eed78c9c8764186148
Ira Miller <public@iramiller.com>
2018-01-08 16:07:27 -0500

    migration ready

--- truffle.js
+++ truffle.js
@@ -1,9 +1,23 @@
 module.exports = {
   networks: {
-    development: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+    homestead:  {
+      network_id: 1,
+      host: "127.0.0.1",
+      port:  8545
+    },
+    ropsten:  {
+      network_id: 3,
+      host: "127.0.0.1",
+      port:  8545
+    },
+    ganache: {
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: 5777
     }
+  // },
+  // rpc: {
+  //   host: '127.0.0.1',
+  //   post:8080
   }
 };
